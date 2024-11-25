0de33863c161a5765b672dd71be7b4bcbc187d08
DK <dimentium@icloud.com>
2024-11-25 10:14:07 +0100

    Update zmk-studio_aarch64.rb

--- Casks/zmk-studio_aarch64.rb
+++ Casks/zmk-studio_aarch64.rb
@@ -2,7 +2,7 @@ cask "zmk-studio_aarch64" do
   version "0.2.4"
   sha256 :no_check
 
-  url "https://github.com/zmkfirmware/zmk-studio/release/download/v#{version}/zmk-studio_#{version}_aarch64.dmg"
+  url "https://github.com/zmkfirmware/zmk-studio/releases/download/v#{version}/zmk-studio_#{version}_aarch64.dmg"
   name "zmk-studio.app"
   desc "ZMK-Studio - keyboard layout editor"
   homepage "https://github.com/zmkfirmware/zmk-studio"
