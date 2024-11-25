8b837ca574bbfbee04a4b6422b3ad4bd6c34408f
DK <dimentium@icloud.com>
2024-11-25 10:39:32 +0100

    Fix arch detect

--- Casks/zmk-studio.rb
+++ Casks/zmk-studio.rb
@@ -1,16 +1,14 @@
 cask "zmk-studio" do
   version "0.2.3"
   sha256 :no_check
+  arch arm: "aarch64", intel: "x64"
 
   livecheck do
     url "https://github.com/zmkfirmware/zmk-studio/tags"
     regex(/^v?(\d+(?:\.\d+)+)$/i)
   end
-
-  on_arm do:
-    url "https://github.com/zmkfirmware/zmk-studio/releases/download/v#{version}/zmk-studio_#{version}_aarch64.dmg"
-  on_intel do:
-    url "https://github.com/zmkfirmware/zmk-studio/releases/download/v#{version}/zmk-studio_#{version}_x64.dmg"
+    
+  url "https://github.com/zmkfirmware/zmk-studio/releases/download/v#{version}/zmk-studio_#{version}_#{arch}.dmg"
   
   name "zmk-studio.app"
   desc "ZMK-Studio - keyboard layout editor"
