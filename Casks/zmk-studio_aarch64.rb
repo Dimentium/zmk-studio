94d79dbbc2f9954c44be3b80801cef1108961b07
DK <dimentium@icloud.com>
2024-11-25 10:27:49 +0100

    Update livecheck

--- Casks/zmk-studio_aarch64.rb
+++ Casks/zmk-studio_aarch64.rb
@@ -2,15 +2,15 @@ cask "zmk-studio_aarch64" do
   version "0.2.3"
   sha256 :no_check
 
-  url "https://github.com/zmkfirmware/zmk-studio/releases/download/v#{version}/zmk-studio_#{version}_aarch64.dmg"
-  name "zmk-studio.app"
-  desc "ZMK-Studio - keyboard layout editor"
-  homepage "https://github.com/zmkfirmware/zmk-studio"
-
   livecheck do
     url "https://github.com/zmkfirmware/zmk-studio/tags"
     regex(/^v?(\d+(?:\.\d+)+)$/i)
   end
+
+  url "https://github.com/zmkfirmware/zmk-studio/releases/download/v#{version}/zmk-studio_#{version}_aarch64.dmg"
+  name "zmk-studio.app"
+  desc "ZMK-Studio - keyboard layout editor"
+  homepage "https://github.com/zmkfirmware/zmk-studio"
 
   depends_on macos: ">= :monterey"
 
